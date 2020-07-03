f6f11702dde4545e18b1a10c6840e4e8a9c5e732
Pouya <pouya.eghbali@yandex.com>
2020-07-03 18:43:44 +0200

    Upgrade ostrio:files

--- package.js
+++ package.js
@@ -17,7 +17,7 @@ Package.onUse(function(api) {
     'reactive-var',
     'templating',
     'aldeed:autoform@6.2.0',
-    'ostrio:files@1.9.0',
+    'ostrio:files',
     'dburles:mongo-collection-instances@0.3.5'
   ]);
 
